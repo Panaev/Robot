e4e9f7558fc182404a8f3e3231b017c8ea0261d5
Panaev <skabichevsky.panaev@gmail.com>
2015-04-02 21:09:24 +0500

    Update Edge.cs

--- Edge.cs
+++ Edge.cs
@@ -3,23 +3,20 @@ namespace RobotDevelopment
   class Edge //ребро
   {
     public readonly Node From;
-	  public readonly Node To;
-	  
-	  public Edge(Node first, Node second)
-	  {
-		  this.From = first;
-		  this.To = second;
-	  }
-	  
-	  public bool IsIncident(Node node)
-	  {
-  		return From == node || To == node;
-	  }
-	  
-	  public Node OtherNode(Node node)
-	  {
-		  if (From == node) return To;
-		  return From;
-	  }
+    public readonly Node To;
+    public Edge(Node first, Node second)
+    {
+    	this.From = first;
+    	this.To = second;
+    }
+    public bool IsIncident(Node node)
+    {
+    	return From == node || To == node;
+    }
+    public Node OtherNode(Node node)
+    {
+    	if (From == node) return To;
+    	return From;
+    }
   }
 }
