1b0e1d098951ed7801c1908f621e017273e2023b
Panaev <skabichevsky.panaev@gmail.com>
2015-04-06 15:08:39 +0500

    Update Robot.cs

--- Robot.cs
+++ Robot.cs
@@ -1,1 +1,17 @@
-
+namespace RobotDevelopment
+{
+  public class Robot
+  {
+    public Pos { get { return Position; } } //как это пишется, поправьте меня
+    
+    public GoTo(double deltaX, double deltaY)
+    {
+      //Робот изменяет свою координату
+    }
+    
+    public Rotate(double alpha) //или он не принимает угол...
+    {
+      //поворот робота
+    }
+  }
+}
