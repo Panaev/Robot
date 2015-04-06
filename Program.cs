d870cdf0a922b9a4c7fe6de35522a0af04478f78
Panaev <skabichevsky.panaev@gmail.com>
2015-04-06 15:16:30 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-
+namespace RobotDevelopment
+{
+  public static class Program
+  {
+    var mainloop = True;
+    Robot robot = new Robot();
+    while (mainloop)// робот работает в "вечном" цикле
+    {
+      // создаётся карта - каждый раз по новой, запихивается в граф
+      robot.GoTo(map);
+    }
+  }
+}
