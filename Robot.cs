883d0515cfd11140092c3f6778f97b16b9302870
Panaev <skabichevsky.panaev@gmail.com>
2015-04-06 15:43:57 +0500

    Update Robot.cs

--- Robot.cs
+++ Robot.cs
@@ -3,17 +3,19 @@ namespace RobotDevelopment
   public class Robot
   {
     public Pos { get { return Position; } } // как это пишется, поправьте меня
-    public DetailsInfo { get { return DetailsInfo; } }
+    public DetailsInfo { get { return DetailsInfo; } } // и это тоже...
     
     public Go()
     {
       // робот едет
+      PositionSensorsData sensorsData = null;
       sensorsData = server.SendCommand(new Command { LinearVelocity = 50, Time = 1 });
     }
     
     public Rotate(double alpha) // или он принимает не угол...
     {
       // поворот робота
+      PositionSensorsData sensorsData = null;
       sensorsData = server.SendCommand(new Command { AngularVelocity = Angle.FromGrad(alpha), Time = 1 });
     }
   }
