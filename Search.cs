7da62b56bf2b78224106df0aa082c410c6023082
NskDrake <drake.norilsk@gmail.com>
2015-04-08 17:05:54 +0500

    Update Search.cs

--- Search.cs
+++ Search.cs
@@ -10,6 +10,33 @@ namespace Robot
 //нам наверно понадобится поиск в ширину, так что первый метод не нужен, он работает, вершины выводятся в правильном порядке
     public class Search 
     {
+        public static List<Node> FindPath(Node start, Node end)
+        {
+            var track = new Dictionary<Node, Node>();
+            track[start] = null;
+            var queue = new Queue<Node>();
+            queue.Enqueue(start);
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+                foreach (var nextNode in node.incidentNodes)
+                {
+                    if (track.ContainsKey(nextNode)) continue;
+                    track[nextNode] = node;
+                    queue.Enqueue(nextNode);
+                }
+                if (track.ContainsKey(end)) break;
+            }
+            var pathItem = end;
+            var result = new List<Node>();
+            while (pathItem != null)
+            {
+                result.Add(pathItem);
+                pathItem = track[pathItem];
+            }
+            result.Reverse();
+            return result;
+        }
         public static IEnumerable<Node> DepthSearch(Node startNode)
         {
             var visited = new HashSet<Node>();
